303299ff8cd0ed5d6f7a1c3cf250cc63be7e34a2
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-05-15 00:55:03 +0600

    ff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,17 +6,17 @@ export default function Home() {
   return (
 <>
 
-<div className=" bg-gray-100 w-full h-full px-10 py-6 ">
+<div className=" bg-gray-100 w-full h-full py-4">
 
 <div className="mx-auto flex justify-center h-full">
-      <div className="w-[95%] shadow-xl border-[6px] border-gray-400  rounded-lg h-screen ">
+      <div className="w-[97%] shadow-xl border-[5px] border-gray-400  rounded-lg h-screen ">
         <div>
           
           
-        <div className=" px-2 bg-gray-300  py-5  mb-5  border-b-[6px] border-white border-double">
-            <h2 className="text-2xl mb-1  text-black text-center  font-bold 
+        <div className=" px-2 bg-gray-300  py-[4px]  mb-5  border-b-[6px] border-white border-double">
+            <h2 className="text-2xl text-[18px] text-black text-center  font-bold 
           ">Welcome to the Backboard</h2> 
-               <h2 className="text-center  font-semibold text-[#5d5f61]">Hira Trusted Trader</h2>
+               <h2 className="text-center text-[14px] font-semibold text-[#5d5f61]">Hira Trusted Trader</h2>
         </div>
 
      
