9f04a60e61416bfeb2e153275ec5d0203ce9895b
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-05-20 16:54:47 +0600

    GG

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,7 @@
 // import Image from "next/image";
+"use client"
+import {motion} from "framer-motion";
+
 
 
 
@@ -6,21 +9,25 @@ export default function Home() {
   return (
 <>
 
-<div className=" bg-gray-100 w-full h-full py-4">
+<div className=" bg-gray-100 w-full h-full py-[3px] ">
 
-<div className="mx-auto flex justify-center h-full">
-      <div className="w-[98%] shadow-xl border-[5px] border-gray-400  rounded-lg min-h-[96.4vh]">
+<motion.div initial={{opacity: 0, y: -100}} transition={{type: "spring", stiffness: 100, damping: 10, delay: 0.4}} animate={{opacity: 1, y: 0}} className="mx-auto flex justify-center h-full">
+      <div className="w-[90%] shadow-2xl border-[4px] border-gray-400  rounded-lg min-h-[99vh]">
         <div>
           
-        <div className=" bg-gray-300   mb-5  border-b-[6px] border-white border-double">
-            <h2 className="text-2xl text-[17px] p-0 text-black text-center  font-bold 
+        <div className=" bg-gray-300 border-b-[4px] border-white border-double">
+            <div>
+              <h2 className=" text-[13px] text-black text-center  font-bold 
           ">Welcome to the Blackboard</h2> 
-               <h2 className="text-center italic pb-1 text-[14px] font-semibold text-[#5d5f61]">Hira Trusted Trader</h2>
+           <p className="text-center  text-[11px] font-semibold text-[#5d5f61] hover:text-green-500 cursor-pointer transition duration-700 ease-in-out">Hira Trusted Trader</p>
+            </div>
+           
+              
         </div>
 
         </div>
   </div>
-</div>
+</motion.div>
 
 
     </div>
